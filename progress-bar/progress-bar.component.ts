823868935ef7f2f062ceafc14be86a7df62e7a88
Sergey Afanasev <hugegarbagecan@gmail.com>
2021-04-15 16:15:56 -0400

    Create progress-bar.component.ts

---
+++ progress-bar/progress-bar.component.ts
@@ -0,0 +1,16 @@
+import {Component, Input, OnInit} from '@angular/core';
+
+@Component({
+  selector: 'progress-bar',
+  templateUrl: './progress-bar.component.html',
+  styleUrls: ['./progress-bar.component.scss']
+})
+export class ProgressBarComponent implements OnInit {
+  @Input()
+  value: number;
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
